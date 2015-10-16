898374a443c09e697d8f4fbc639271ecc50cac59
Tim <timrobertson0122@gmail.com>
2015-10-16 11:41:51 +0200

    all methods present, raw_color,item_gender_item_type still in progress

--- oxygendemo/spiders/oxygen.py
+++ oxygendemo/spiders/oxygen.py
@@ -16,19 +16,13 @@ class OxygenSpider(CrawlSpider):
 #   # to find appropriate category listing pages,
 #   # to identify individual product pages (this rule should have a callback='parse_item'),
     rules = (
-        Rule(LinkExtractor(allow=('boots.aspx', ), deny=('designers.aspx', ))),
+        # Rule(LinkExtractor(allow=('Sale-In.aspx', ), deny=('designers.aspx', ))),
         # Extract links matching 'item.php' and parse them with the spider's method parse_item
-        Rule(LinkExtractor(allow=('Running-sneaker.aspx', )), callback='parse_item'),
+        Rule(LinkExtractor(allow=('Leather-Trim-Slouch-Pant.aspx', )), callback='parse_item'),
     )
 
-    def parse_item(self, response):
-        pq = PyQuery(response.body)
-        item = OxygendemoItem()
-        item['designer'] = self.item_designer(pq)
-        item['name'] = self.item_name(pq)
-        item['link'] = response.url
-        
-        return item
+    def item_code(self, response):
+        return response.url.split('/')[-1].split('.')[0].lower()
 
     def item_designer(self, pq):
         return pq('.brand_name a').text()
@@ -36,5 +30,62 @@ class OxygenSpider(CrawlSpider):
     def item_name(self, pq):
         return pq('h2').text()
 
-    def item_url(self, pq):
+    def item_url(self, response):
         return response.url
+
+    def item_gbp_price(self, pq):
+        return pq('.price').text().split(' ')[-1]
+
+    def item_sale_discount(self, pq):
+        original_price_int = eval(pq('.offsetMark').text())
+        sale_price_int = eval(pq('.price').text().split(' ')[-1])
+        discount_percent = (sale_price_int / original_price_int) * 100
+        return discount_percent
+
+    def item_description(self, pq):
+        return pq('h3').eq(0).next().text()
+
+    def item_images(self, pq):
+        img_array = []
+        for i in pq('img[src*="GetImage"]'):
+          img_array.append(i.attrib["src"])
+        return img_array
+     
+    def item_stock_status(self, pq):
+        stock_dict = {}
+        for i in pq('.productpage_box option'):
+          if any(ch.isdigit() for ch in i.text): 
+            if i.text.isdigit():
+              stock_dict.update({i.text:3})
+            else:
+              d = i.text.split(' ')[0]
+              stock_dict.update({d:1})
+        return stock_dict  
+
+    def item_raw_color(self, pq):
+      return 'None'
+
+    def item_gender(self, pq):
+      return 'F'
+
+    def item_type(self, pq):
+      return 'A'
+
+    def parse_item(self, response):
+        pq = PyQuery(response.body)
+        item = OxygendemoItem()
+        item['code'] = self.item_code(response)
+        item['designer'] = self.item_designer(pq)
+        item['name'] = self.item_name(pq)
+        item['link'] = self.item_url(response)
+        item['gbp_price'] = self.item_gbp_price(pq)
+        item['raw_color'] = self.item_raw_color(pq)
+        item['gender'] = self.item_gender(pq)
+        item['description'] = self.item_description(pq)
+        item['sale_discount'] = self.item_sale_discount(pq)
+        item['images'] = self.item_images(pq)
+        item['stock_status'] = self.item_stock_status(pq)
+        item['type'] = self.item_type(pq)
+        return item
+
+    
